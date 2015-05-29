be11bd46f7f90805b7db39ccf4f4b56d618b6f7d
rgowan <rane_00@hotmail.com>
2015-05-29 17:00:35 +0100

    added next button on product index to cycle through pages

--- app/assets/javascripts/views/products/index.js
+++ app/assets/javascripts/views/products/index.js
@@ -9,15 +9,30 @@ define([
     var ProductsIndexView = Backbone.View.extend({
       el: 'main',
       initialize: function(){
+        this.currentPage = 1; // or whateva
         var self = this;
         var collection = new ProductsCollection();
         collection.getPage(1).done(function(data){
           self.render(data)
-        })
+        });
       },
       render: function(data){
         var template = _.template(ProductsTemplate);
         this.$el.html(template({products: data}));
+      },
+
+      events: {
+        "click #next-page": "nextPage"
+      },
+
+      nextPage: function(event){
+        event.preventDefault();
+        var self = this;
+        var collection = new ProductsCollection();
+        collection.getPage(this.currentPage).done(function(data){
+          self.currentPage++
+          self.render(data)
+        });
       }
     });
 
