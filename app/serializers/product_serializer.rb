8476c2425d0113087d998220ff5104248def9b17
rgowan <rane_00@hotmail.com>
2015-06-04 11:19:19 +0100

    added likes/dislikes in json object response for products

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,12 @@
 class ProductSerializer < ActiveModel::Serializer
   has_one :wish, except: [:products]
-  attributes :id, :name, :image_url_large, :description, :url, :price_retail
+  attributes :id, :name, :image_url_large, :description, :url, :price_retail, :likes, :dislikes
+
+  def likes
+    object.get_likes.size
+  end
+
+  def dislikes
+    object.get_dislikes.size
+  end
 end
