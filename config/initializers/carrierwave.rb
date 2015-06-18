4150d9b5d392e679515d081a91c69a336e5f739e
rgowan <rane_00@hotmail.com>
2015-06-18 11:28:01 +0100

    added access keys into code via env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',                                # required
-    aws_access_key_id: '<your AWSAccessKeyId>',     # required
-    aws_secret_access_key: '<your AWSSecretKey>',   # required
+    aws_access_key_id: ENV['SHOE_SELECTOR_AWS_ACCESS_KEY'],     # required
+    aws_secret_access_key: ENV['SHOE_SELECTOR_AWS_SECRET_KEY'], # required
     region: 'eu-west-1',                            # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = '<your bucket name>'      # required
+  config.fog_directory  = ENV['SHOE_SELECTOR_AWS_BUCKET_NAME'] # required
   config.fog_public  = true                         # optional, defaults to true
 end
