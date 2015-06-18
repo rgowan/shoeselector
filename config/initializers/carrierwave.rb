7827dcd444d17ef9dcdf181e575b34eb3ad7114a
rgowan <rane_00@hotmail.com>
2015-06-18 11:34:48 +0100

    sourced zshrc changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',                                # required
-    aws_access_key_id: ENV['SHOE_SELECTOR_AWS_SECRET_KEY'],     # required
-    aws_secret_access_key: ENV['SHOE_SELECTOR_AWS_ACCESS_KEY'], # required
+    aws_access_key_id: ENV['SHOE_SELECTOR_AWS_ACCESS_KEY'],     # required
+    aws_secret_access_key: ENV['SHOE_SELECTOR_AWS_SECRET_ACCESS_KEY'], # required
     region: 'eu-west-1',                            # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = ENV['SHOE_SELECTOR_AWS_BUCKET_NAME'] # required
